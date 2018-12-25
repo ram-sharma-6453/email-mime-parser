d9d147e5fd11581d2746d9847574eb097f24f12d
ram-sharma-6453 <ram.sharma.6453@gmail.com>
2018-12-25 21:40:20 +0530

    Updated the documentation.

--- src/main/java/tech/blueglacier/parser/CustomContentHandler.java
+++ src/main/java/tech/blueglacier/parser/CustomContentHandler.java
@@ -13,52 +13,52 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class CustomContentHandler extends AbstractContentHandler {
-	
-	private Email email;
 
-	public CustomContentHandler(){
-		this.email = new Email();
-	}	
-	
-	public Email getEmail() {
-		return email;
-	}
+    private Email email;
 
-	@Override
+    public CustomContentHandler() {
+        this.email = new Email();
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    @Override
     public void field(Field field) throws MimeException {
-		email.getHeader().addField(field);
-    }	
-	
-	@Override
-	public void body(BodyDescriptor bd, InputStream is) throws MimeException, IOException {
-		// Gracefully switiching off the case of tech.blueglacier.email attached within an tech.blueglacier.email
-		if(email.getMessageStack().peek().getEmailMessageTypeHierarchy() == EmailMessageTypeHierarchy.parent){
-		email.fillEmailContents(bd, is);
-	}
-	}
-	
-	@Override
-	public void startMessage() throws MimeException {
-		if(email.getMessageStack().empty()){
-			email.getMessageStack().push(new EmailMessageType(EmailMessageTypeHierarchy.parent));
-		}else{
-			email.getMessageStack().push(new EmailMessageType(EmailMessageTypeHierarchy.child));
-		}
-	}
-	
-	@Override
-	public void endMessage(){
-		if(email.getMessageStack().peek().getEmailMessageTypeHierarchy() == EmailMessageTypeHierarchy.parent){
-		email.reArrangeEmail();
-	}
-		email.getMessageStack().pop();
-	}
-	
-	public void endMultipart() throws MimeException {
-		email.getMultipartStack().pop();
-	}
+        email.getHeader().addField(field);
+    }
 
-	public void startMultipart(BodyDescriptor bd) throws MimeException {
-		email.getMultipartStack().push(new MultipartType(bd));
-	}
+    @Override
+    public void body(BodyDescriptor bd, InputStream is) throws MimeException, IOException {
+        // Gracefully switching off the case of email attached within an email
+        if (email.getMessageStack().peek().getEmailMessageTypeHierarchy() == EmailMessageTypeHierarchy.parent) {
+            email.fillEmailContents(bd, is);
+        }
+    }
+
+    @Override
+    public void startMessage() throws MimeException {
+        if (email.getMessageStack().empty()) {
+            email.getMessageStack().push(new EmailMessageType(EmailMessageTypeHierarchy.parent));
+        } else {
+            email.getMessageStack().push(new EmailMessageType(EmailMessageTypeHierarchy.child));
+        }
+    }
+
+    @Override
+    public void endMessage() {
+        if (email.getMessageStack().peek().getEmailMessageTypeHierarchy() == EmailMessageTypeHierarchy.parent) {
+            email.reArrangeEmail();
+        }
+        email.getMessageStack().pop();
+    }
+
+    public void endMultipart() throws MimeException {
+        email.getMultipartStack().pop();
+    }
+
+    public void startMultipart(BodyDescriptor bd) throws MimeException {
+        email.getMultipartStack().push(new MultipartType(bd));
+    }
 }
